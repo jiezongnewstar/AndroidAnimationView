8d7644d702402ff527dc8899387135da69b774e0
xibei <542731522@qq.com>
2019-01-22 16:45:07 +0800

    AnimatorSet 用法

---
+++ app/src/main/java/com/xibei/personaldesign/itemDemo/ActivityAnimation12.java
@@ -0,0 +1,20 @@
+package com.xibei.personaldesign.itemDemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import com.xibei.personaldesign.R;
+
+public class ActivityAnimation12 extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_animation_12);
+
+
+    }
+
+
+}
